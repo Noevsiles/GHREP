710b55a6eb87220e5e4e16e1e3854da09b33e80c
Alumno_Presencial <Alumno_Presencial@DESKTOP-B44B142.mshome.net>
2023-11-24 09:53:47 +0100

    primer commit en github

--- ejemploGit/src/main/java/ejemploGit/principal.java
+++ ejemploGit/src/main/java/ejemploGit/principal.java
@@ -5,8 +5,12 @@ public class principal {
 	public static void main(String[] args) {
 		System.out.println("hola git");
 		
-		
 		System.out.println("ya se usar git");
+		
+		String miVariable;
+		String miEntero;
+		
+		System.out.println("Ahora en GitHub");
 
 	}
 
